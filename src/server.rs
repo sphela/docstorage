61f3fb53d1cd7dc940e4705aabd1041abd0bf949
Bjorn Tipling <bjorn@ambientchill.com>
2018-11-10 07:12:07 -0800

    Use Arc/Mutex to go multithreaded

--- src/server.rs
+++ src/server.rs
@@ -3,14 +3,12 @@ use configure;
 use std::fs::File;
 use std::io::prelude::*;
 
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 use hyper::{Body, Response, Server, StatusCode};
 use hyper::header::{HeaderValue};
 use hyper::service::service_fn_ok;
-use hyper::rt::Future;
-use tokio::runtime::current_thread;
+use hyper::rt::{self, Future};
 
 pub fn run (config: &configure::Config) {
     let doc_root = config.server.doc_root.clone();
@@ -21,11 +19,12 @@ pub fn run (config: &configure::Config) {
     let mut html_contents = String::new();
     f.read_to_string(&mut html_contents)
         .expect(&format!("something went wrong reading the {} file", index_path));
-    let html_contents = Rc::new(RefCell::new(html_contents));
+
+    let html_contents = Arc::new(Mutex::new(html_contents));
     let new_service = move || {
         let html_c = html_contents.clone();
             service_fn_ok( move |_| {
-                let c = html_c.borrow_mut();
+                let c = html_c.lock().unwrap();
                 let mut response: Response<Body> = Response::default();
                 *response.status_mut() = StatusCode::OK;
                 response.headers_mut().insert("X-Thank-You", HeaderValue::from_static("For using Sphela!"));
@@ -34,18 +33,11 @@ pub fn run (config: &configure::Config) {
         })
     };
 
-    let exec = current_thread::TaskExecutor::current();
-
     let server = Server::bind(&addr)
-        .executor(exec)
         .serve(new_service)
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", addr);
 
-    current_thread::Runtime::new()
-        .expect("rt new")
-        .spawn(server)
-        .run()
-        .expect("rt run");
+    rt::run(server);
 }
